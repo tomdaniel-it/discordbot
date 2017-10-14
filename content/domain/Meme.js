a259ed702f5c6a9c2a6c4dc4b10a3547753e9d9f
thecoder <thecoder@iswleuven.be>
2017-10-14 17:41:59 +0200

    Fixed Meme command bug

--- content/domain/Meme.js
+++ content/domain/Meme.js
@@ -2,7 +2,19 @@ module.exports = {
     execute: function(command){
         var fs = require('fs');
         var filenames = fs.readdirSync('content/storage/memes');
-        var filename = filenames[Math.floor(Math.random()*filenames.length)];
+        if(filenames.length===0){
+            //NO IMAGES IN DIRECTORY
+            command.getMessage().channel.send("Error: content/storage/memes/ does not contain any meme images.");
+            return;
+        }else if(filenames.length===1 && filenames[0]==="meme.jpg-example"){
+            //NO IMAGES IN DIRECTORY EXCEPT EXAMPLE
+            command.getMessage().channel.send("Error: content/storage/memes/ does not contain any meme images.");
+            return;
+        }
+        var filename = "meme.jpg-example";
+        while(filename==="meme.jpg-example"){
+            filename = filenames[Math.floor(Math.random()*filenames.length)];
+        }
         command.getMessage().channel.send("", {file:"content/storage/memes/"+filename});
 
     }
