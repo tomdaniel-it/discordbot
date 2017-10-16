ab2802c08a3056fdd076b4dd6b43ad114cba8b1b
thecoder <thecoder@iswleuven.be>
2017-10-16 19:18:36 +0200

    Added invite link to settings

--- settings.js
+++ settings.js
@@ -2,4 +2,5 @@ module.exports = {
     error_message_time_until_deletion: 5, //IN SECONDS
     memes_directory: __dirname + "/content/storage/memes/",
     command_prefix: ".", //SINGLE CHAR REQUIRED!
+    discord_invite: "https://discordapp.com/oauth2/authorize?client_id=368465430924230670&scope=bot&permissions=2146958591",
 };
