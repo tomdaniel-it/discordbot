385e749558ebf7a42180b01d2e664d0de94ac1aa
thecoder <thecoder@iswleuven.be>
2017-11-01 02:01:54 +0100

    Added Poll_Remind.js command

---
+++ content/domain/commands/polls/Poll_Remind.js
@@ -0,0 +1,23 @@
+var pollmanager = require('../../PollManager.js');
+var genericfunctions = require('../../GenericFunctions.js');
+
+module.exports = {
+    execute: function(command){
+        var guild = command.getMessage().guild;
+        var serverid = null;
+        var userid = command.getMessage().author.id;
+
+        if(guild === undefined || guild === null){
+            genericfunctions.sendErrorMessage(command, "This command is only available in a discord server.");
+            return;
+        }
+        var serverid = guild.id;
+        if(guild.id === undefined || guild.id === null || guild.id.length === 0){
+            genericfunctions.sendErrorMessage(command, "This command is only available in a discord server.");
+            return;
+        }
+
+        var name = command.getParameters()[0].value;
+        genericfunctions.pollSendMessage(serverid, name, command);
+    }
+};
