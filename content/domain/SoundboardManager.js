7d080658c838aa7dc54d5480ec3daa6fba57bcce
thecoder <thecoder@iswleuven.be>
2017-11-10 19:39:52 +0100

    Added SoundboardManager.js

--- content/domain/SoundboardManager.js
+++ content/domain/SoundboardManager.js
@@ -38,19 +38,107 @@ var options = {
     }
 };
 
+function isStandaloneSound(sound){
+    var em = new emitter();
+    setTimeout(function(){
+        if(sound === undefined || sound === null || sound === ""){
+            em.emit('ready', false);
+            return;
+        }
+        if(sound.slice(-1) === "/") sound = sound.substring(0, sound.length-1);
+        module.exports.getAll().on('ready', list=>{
+            for(var i=0;i<list.length;i++){
+                if(list[i].slice(-1) === "/") continue;
+                if(list[i].toLowerCase() === sound.trim().toLowerCase()){
+                    em.emit("ready", true);
+                    return;
+                }
+            }
+            em.emit("ready", false);
+            return;
+        });
+    }, 1);
+    return em;
+}
+
 module.exports = {
     get: function(directory){
         var em = new emitter();
         setTimeout(function(){
             if(directory === undefined) directory = null;
-            var newoptions = JSON.parse(JSON.stringify(options));
             if(directory !== null){
-                newoptions.path = newoptions.path.replace("{sound}", directory);
-            }else{
-                newoptions.path = newoptions.path.replace("{sound}", "");
+                directory = directory.trim();
+                if(directory === ""){
+                    directory = null;
+                }
             }
+            isStandaloneSound(directory).on("ready", result=>{
+                if(result){
+                    em.emit("ready", [], new Error("You can only list a directory or everything, not a single sound."));
+                    return;
+                }
+                if(directory !== null && directory.indexOf("/") !== -1 && directory.slice(-1) !== "/"){
+                    em.emit("ready", [], new Error("You can only list a directory or everything, not a single sound."));
+                    return;
+                }
+                var newoptions = JSON.parse(JSON.stringify(options));
+                if(directory !== null){
+                    newoptions.path = newoptions.path.replace("{sound}", directory);
+                }else{
+                    newoptions.path = newoptions.path.replace("{sound}", "");
+                }
+                getJSON(newoptions, (statusCode, result)=>{
+                    if(result.length===0){
+                        em.emit("ready", [], new Error("That is an unknown directory."));
+                        return;
+                    }
+                    var newlist = [];
+                    for(var i=0;i<result.length;i++){
+                        if(directory !== null){
+                            newlist.push(result[i].urlSnip);
+                        }else{
+                            var regex = /.*\/.*/;  //test/test
+                            if(regex.test(result[i].urlSnip)){
+                                //item is in directory
+                                var item = result[i].urlSnip.split("/")[0]+"/";
+                                if(newlist.indexOf(item) === -1){
+                                    newlist.push(item);
+                                }
+                            }else{
+                                //item is NOT in directory
+                                newlist.push(result[i].urlSnip);
+                            }
+                        }
+                    }
+                    var directoryList = [];
+                    var itemList = [];
+                    for(var i=0;i<newlist.length;i++){
+                        if(newlist[i].slice(-1) === "/"){
+                            directoryList.push(newlist[i]);
+                        }else{
+                            itemList.push(newlist[i]);
+                        }
+                    }
+                    directoryList = directoryList.sort();
+                    itemList = itemList.sort();
+                    newlist = directoryList.concat(itemList);
+                    em.emit("ready", newlist);
+                });
+            });
+        }, 1);
+        return em;
+    },
+    getAll: function(){
+        var em = new emitter();
+        setTimeout(function(){
+            var newoptions = JSON.parse(JSON.stringify(options));
+            newoptions.path = newoptions.path.replace("{sound}", "");
             getJSON(newoptions, (statusCode, result)=>{
-                em.emit("ready", result);
+                var newlist = [];
+                for(var i=0;i<result.length;i++){
+                    newlist.push(result[i].urlSnip);
+                }
+                em.emit("ready", newlist);
             });
         }, 1);
         return em;
@@ -58,9 +146,17 @@ module.exports = {
     play: function(sound){
         var em = new emitter();
         setTimeout(function(){
+            if(sound === undefined || sound === null || sound.trim() === ""){
+                em.emit("ready", [], new Error("Enter a sound to play."));
+                return;
+            }
             var newoptions = JSON.parse(JSON.stringify(options));
             newoptions.path = newoptions.path.replace("{sound}", sound);
             getJSON(newoptions, (statusCode, result)=>{
+                if(result.length === 0){
+                    em.emit("ready", [], new Error("That is an unknown sound."));
+                    return;
+                }
                 em.emit("ready", result);
             });
         }, 1);
