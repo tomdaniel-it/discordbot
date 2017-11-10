24e9d8d6df9727d4c43b2592d0d810b304fe894c
thecoder <thecoder@iswleuven.be>
2017-11-10 19:40:18 +0100

    Added Sb_Play.js command

--- content/domain/commands/soundboard/Sb_Play.js
+++ content/domain/commands/soundboard/Sb_Play.js
@@ -1,5 +1,23 @@
+var soundboardmanager = require('../../SoundboardManager.js');
+var genericfunctions = require('../../GenericFunctions.js');
+
 module.exports = {
     execute: function(command){
-        
+        var sound = command.getParameters();
+        soundboardmanager.getAll().on('ready', result=>{
+            for(var i=0;i<result.length;i++){
+                if(result[i].toLowerCase() === sound.toLowerCase()){
+                    sound = result[i];
+                    break;
+                }
+            }
+            soundboardmanager.play(sound).on('ready', (result, error)=>{
+                if(error){
+                    genericfunctions.sendErrorMessage(command, error.message);
+                    return;
+                }
+                genericfunctions.deleteMessage(command.getMessage());
+            });
+        });
     }
 };
